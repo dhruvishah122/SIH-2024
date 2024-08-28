fb212e9fb39dfb7376d36dd92e2c15a7481be15c
Shreya-1305 <paintershreya@gmail.com>
2024-08-28 17:59:42 +0530

    post added

--- Frontend/src/Post/PostPage.jsx
+++ Frontend/src/Post/PostPage.jsx
@@ -4,46 +4,21 @@ import NavBar from "../components/NavBar";
 
 const Base_URL = "http://localhost:700/";
 function PostPage() {
-  const [posts, setPosts] = useState([
-    {
-      name: "Dhruvi",
-      id: "66cdc343b34e09ef2fe77303",
-      content: "hello",
-      email: "amazon@gmail.com",
-      _id: "66ced6c3c580deaab2086b72",
-      __v: 0,
-    },
-    {
-      name: "Dhruvi",
-      id: "66cdc343b34e09ef2fe77303",
-      content: "hello",
-      email: "amazon@gmail.com",
-      _id: "66ced6c3c580deaab2086b72",
-      __v: 0,
-    },
-    {
-      name: "Dhruvi",
-      id: "66cdc343b34e09ef2fe77303",
-      content: "hello",
-      email: "amazon@gmail.com",
-      _id: "66ced6c3c580deaab2086b72",
-      __v: 0,
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
 
-  //   useEffect(function () {
-  //     async function FetchInvestors() {
-  //       try {
-  //         const res = await fetch(`${Base_URL}posts`);
-  //         const data = await res.json();
-  //         console.log(data);
-  //         setInvestors(data);
-  //       } catch {
-  //         alert("There was an error loading data");
-  //       }
-  //     }
-  //     FetchInvestors();
-  //   }, []);
+  useEffect(function () {
+    async function FetchInvestors() {
+      try {
+        const res = await fetch(`${Base_URL}posts`);
+        const data = await res.json();
+        console.log(data);
+        setPosts(data);
+      } catch {
+        alert("There was an error loading data");
+      }
+    }
+    FetchInvestors();
+  }, []);
   return (
     <div>
       <NavBar />
