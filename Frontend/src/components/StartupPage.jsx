4c1ee0ea3c6b8a302f17783022767b2a6f35028f
Shreya-1305 <paintershreya@gmail.com>
2024-09-03 23:19:06 +0530

    search feature added

--- Frontend/src/components/StartupPage.jsx
+++ Frontend/src/components/StartupPage.jsx
@@ -6,6 +6,25 @@ const Base_URL = "http://localhost:900/";
 function StartupPage() {
   const [startups, setStartups] = useState([]);
 
+  const [query, setQuery] = useState("");
+  const [filteredStartups, setFilteredStartups] = useState([]);
+
+  function handleSearch(e) {
+    setQuery(() => e.target.value);
+    if (query.length === 0) setFilteredStartups(() => []);
+    const filteredData = startups.filter((startup) =>
+      `${startup.name} ${startup.technology} ${startup.Industry_Focus}`
+        .toLowerCase()
+        .includes(query.toLowerCase())
+    );
+    console.log(filteredData);
+    setFilteredStartups(() => filteredData);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   useEffect(function () {
     async function FetchStartups() {
       try {
@@ -22,8 +41,66 @@ function StartupPage() {
   return (
     <div>
       <NavBar />
-      {startups.length != 0 && <Startups startups={startups} />}
+      <SearchBox
+        query={query}
+        handleSearch={handleSearch}
+        handleSubmit={handleSubmit}
+        num={filteredStartups.length}
+        total={startups.length}
+      />
+      {startups.length != 0 && (
+        <Startups startups={query.length ? filteredStartups : startups} />
+      )}
     </div>
+  );
+}
+
+function SearchBox({ query, handleSearch, handleSubmit, num, total }) {
+  const buttonStyles = {
+    color: "black",
+    border: "2px solid #d2631f",
+    fontWeight: "500",
+  };
+
+  const buttonStyless = {
+    color: "black",
+    border: "2px solid #d2631f",
+    fontWeight: "500",
+  };
+
+  return (
+    <form
+      className="d-flex gap-4"
+      style={{ margin: "6rem 5rem 0 5rem" }}
+      onSubmit={handleSubmit}
+    >
+      <input
+        className="form-control me-2 "
+        type="search"
+        placeholder="Search"
+        aria-label="Search"
+        style={buttonStyless}
+        value={query}
+        onChange={handleSearch}
+      />
+      <button className="btn button" style={buttonStyles}>
+        Search
+      </button>
+
+      <div
+        style={{
+          width: "7rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          fontSize: "1rem",
+          color: "black",
+          fontWeight: "500",
+        }}
+      >
+        {query.length !== 0 ? `${num} Results` : `${total} startups`}
+      </div>
+    </form>
   );
 }
 
