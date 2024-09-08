231f2efef6689f024c62517ac01e00b55cad6e77
Shreya-1305 <paintershreya@gmail.com>
2024-09-08 22:23:00 +0530

    status updating

--- Frontend/src/DashBoard/TrackingCard.jsx
+++ Frontend/src/DashBoard/TrackingCard.jsx
@@ -1,22 +1,22 @@
 import "./TrackingCard.css";
 
-const startup = {
-  name: "Hopscotch",
-  email: "hopscotch@gmail.com",
-  password: "123",
-  technology:
-    "Artificial Intelligence (AI), Consumer Apps, Enterprise Apps, AdTech, AgriTech, EdTech, FinTech, HealthTech, RetailTech, GreenTech/CleanTech, LogisticsTech, FoodTech, D2C",
-  Industry_Focus:
-    "Advertising,Media & EntertainmentTraining,Energy & Utilities,Healthcare & Pharmaceuticals,IT & ITES,Manufacturing,Retail,Telecom,Transportation & Logistics,Travel & Hospitality",
-  Startup_eligibility_criteria: "VC Funded startups",
-  Startup_Revenue_Preference: 7000000,
-  location: "Ahmedabad, Gujarat, IND",
-  date: "06-09-2024",
-  status: "Funds-sanctioned",
-  funds_sanctioned: 0,
-  _id: "66db3e1270fc7617f926c888",
-  __v: 0,
-};
+// const startup = {
+//   name: "Hopscotch",
+//   email: "hopscotch@gmail.com",
+//   password: "123",
+//   technology:
+//     "Artificial Intelligence (AI), Consumer Apps, Enterprise Apps, AdTech, AgriTech, EdTech, FinTech, HealthTech, RetailTech, GreenTech/CleanTech, LogisticsTech, FoodTech, D2C",
+//   Industry_Focus:
+//     "Advertising,Media & EntertainmentTraining,Energy & Utilities,Healthcare & Pharmaceuticals,IT & ITES,Manufacturing,Retail,Telecom,Transportation & Logistics,Travel & Hospitality",
+//   Startup_eligibility_criteria: "VC Funded startups",
+//   Startup_Revenue_Preference: 7000000,
+//   location: "Ahmedabad, Gujarat, IND",
+//   date: "06-09-2024",
+//   status: "Funds-sanctioned",
+//   funds_sanctioned: 0,
+//   _id: "66db3e1270fc7617f926c888",
+//   __v: 0,
+// };
 
 function TrackingCard({ startup }) {
   return (
@@ -26,8 +26,8 @@ function TrackingCard({ startup }) {
           <div
             className={`step ${
               (startup.status === "Registered" ||
-                startup.status === "In progress" ||
-                startup.status === "Accepted" ||
+                startup.status === "In Progress" ||
+                startup.status === "Approved" ||
                 startup.status === "Rejected" ||
                 startup.status === "Changes Required" ||
                 startup.status === "Funds Transfered") &&
@@ -44,8 +44,8 @@ function TrackingCard({ startup }) {
 
           <div
             className={`step ${
-              (startup.status === "In progress" ||
-                startup.status === "Accepted" ||
+              (startup.status === "In Progress" ||
+                startup.status === "Approved" ||
                 startup.status === "Rejected" ||
                 startup.status === "Changes Required" ||
                 startup.status === "Funds Transfered") &&
@@ -61,7 +61,7 @@ function TrackingCard({ startup }) {
           </div>
           <div
             className={`step ${
-              (startup.status === "Accepted" ||
+              (startup.status === "Approved" ||
                 startup.status === "Rejected" ||
                 startup.status === "Changes Required" ||
                 startup.status === "Funds Transfered") &&
